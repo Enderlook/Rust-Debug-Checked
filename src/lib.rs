9d96748a14f8c92c986f6d3d0ac74f4afe1dd902
Enderlook <enderlook@gmail.com>
2024-07-23 22:36:43 -0300

    Add additional functions

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,116 @@ pub use dc_ref_cell::*;
 pub use dc_option::*;
 pub use dc_result::*;
 pub use dc_slice::*;
+
+/// Replaces the value in `reference` with a new one produced in `closure`.
+///
+/// # Abort (Debug)
+///
+/// It aborts if `closure` panics.
+///
+/// # Safety
+///
+/// `closure` shouldn't panic.
+///
+/// Failing this produces undefined behavior on Release.
+#[cfg(debug_assertions)]
+#[inline(always)]
+pub unsafe fn replace_with_dc<T>(reference: &mut T, closure: impl FnOnce(T) -> T) {
+    use std::{panic, ptr};
+
+    let old_value = ptr::read(reference);
+    let new_value = panic::catch_unwind(panic::AssertUnwindSafe(|| closure(old_value)))
+        .unwrap_or_else(|_| ::std::process::abort());
+    ptr::write(reference, new_value);
+}
+
+/// Replaces the value in `reference` with a new one produced in `closure`.
+///
+/// # Abort (Debug)
+///
+/// It aborts if `closure` panics.
+///
+/// # Safety
+///
+/// `closure` shouldn't panic.
+///
+/// Failing this produces undefined behavior on Release.
+#[cfg(not(debug_assertions))]
+#[inline(always)]
+pub unsafe fn replace_with_dc<T>(reference: &mut T, closure: impl FnOnce(T) -> T) {
+    let old_value = ptr::read(reference);
+    let new_value = closure(old_value);
+    ptr::write(reference, new_value);
+}
+
+/// Informs the compiler this method is never reached.
+///
+/// # Panics (Debug)
+///
+/// Panics if the function is called.
+///
+/// # Safety
+///
+/// Function should never be reached.
+///
+/// Failing this produces undefined behavior on Release.
+#[cfg(debug_assertions)]
+#[track_caller]
+pub unsafe fn unreachable_dc() -> ! {
+    unreachable!();
+}
+
+/// Informs the compiler this method is never reached.
+///
+/// # Panics (Debug)
+///
+/// Panics with the specified message if the function is called.
+///
+/// # Safety
+///
+/// Function should never be reached.
+///
+/// Failing this produces undefined behavior on Release.
+#[cfg(debug_assertions)]
+#[track_caller]
+pub unsafe fn expect_unreachable_dc(msg: &str) -> ! {
+    unreachable!("{}", msg);
+}
+
+/// Informs the compiler this method is never reached.
+///
+/// # Panics (Debug)
+///
+/// Panics if the function is called.
+///
+/// # Safety
+///
+/// Function should never be reached.
+///
+/// Failing this produces undefined behavior on Release.
+#[cfg(not(debug_assertions))]
+#[inline(always)]
+pub unsafe fn unreachable_dc() -> ! {
+    unsafe {
+        std::hint::unreachable_unchecked();
+    }
+}
+
+/// Informs the compiler this method is never reached.
+///
+/// # Panics (Debug)
+///
+/// Panics with the specified message if the function is called.
+///
+/// # Safety
+///
+/// Function should never be reached.
+///
+/// Failing this produces undefined behavior on Release.
+#[cfg(not(debug_assertions))]
+#[inline(always)]
+pub unsafe fn expect_unreachable_dc(_msg: &str) -> ! {
+    unsafe {
+        std::hint::unreachable_unchecked();
+    }
+}
